122586668cdfddfb9480cdb2e3cc794706b969d9
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-06-15 14:51:06 +0500

    Add the required changes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-  belongs_to :user, foreign_key: :user_id
-  belongs_to :post, foreign_key: :post_id
+  belongs_to :user
+  belongs_to :post
 
   after_save :update_like_counter
 
