6e6a26978e28504a1f4500c0fd6adb1ee5bd37f1
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-06-15 11:17:53 +0500

    Add the model classes and methods

---
+++ app/models/like.rb
@@ -0,0 +1,10 @@
+class Like < ApplicationRecord
+    belongs_to :user, foreign_key: :user_id
+    belongs_to :post, foreign_key: :post_id
+
+    after_save :update_like_counter
+
+    def update_like_counter
+        post.increment!(:likes_counter)
+    end
+end
