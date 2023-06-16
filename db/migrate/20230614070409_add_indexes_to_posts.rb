8665431b6b6d6a94b8516ea62a5e5925c93abccf
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-06-16 21:27:19 +0500

    Reset the database

--- db/migrate/20230614070409_add_indexes_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexesToPosts < ActiveRecord::Migration[7.0]
-  def change
-    add_index :posts, :user_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_15_093444) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_14_054039) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-  create_table "likes", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_likes_on_post_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -55,7 +46,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_15_093444) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
-  add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users", column: "author_id"
 end
