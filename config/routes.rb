26ce5369960be5925fa5529097055b5c2d8152dc
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-06-19 23:00:38 +0500

    Add the routes inside routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+ 
+#  Writing routes using the regular method
+  # get '/users' to: 'users#index'
+  # get '/users/:id' to: 'users#show'
+  # get '/users/:id/posts' to: 'posts#index'
+  # get 'users/:id/posts/:id' to 'posts#show'
+
+  # Writing routes using nested method
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
