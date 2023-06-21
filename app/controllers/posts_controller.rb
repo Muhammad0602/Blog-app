e8278cdf23b1ffc036e4a9342e9adfe773d0761a
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-06-22 03:15:34 +0500

    Add all the views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,12 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
+    @posts = Post.where(author_id: @user.id)
   end
 
   def show
     @post = Post.find(params[:id])
+    # @user = User.find(params[:user_id])
+    # @post = Post.where(author_id: @user.id).find(params[:id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @posts = Post.where(author_id: @user.id)
   end
 end
