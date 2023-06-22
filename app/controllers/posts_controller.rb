37672704955bcb55f567284d254493d027312f28
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-06-23 02:01:53 +0500

    Add the form for creating a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,27 @@ class PostsController < ApplicationController
     # @user = User.find(params[:user_id])
     # @post = Post.where(author_id: @user.id).find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    # @post = current_user.posts.build(post_params)
+    
+    @post = Post.new(author_id: current_user.id, **post_params)
+
+    if @post.save
+      flash[:success] = 'Post created successfully' 
+      redirect_to user_posts_path(params[:user_id])
+    else
+      flash[:alert] = 'Something went wrong. Please try again!'
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'users#index'
   
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:new, :create]
+    end
   end
-
-  root 'users#index'
-
-  # get '/users', to: 'users#index', as: 'users'
-  # get '/users/:id', to: 'users#show', as: 'user'
-  # get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  # get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
 end
