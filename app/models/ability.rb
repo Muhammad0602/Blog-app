9db2373ad1b7ed82d342c1086f92447194a96d1d
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-06-30 22:02:42 +0500

    Github issues

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the user here. For example:
+    # Define abilitie for the user here. For example:
 
     return unless user.present?
 
